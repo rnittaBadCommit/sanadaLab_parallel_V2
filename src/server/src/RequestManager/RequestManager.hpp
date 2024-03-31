8db45eb8476be1ab44f2de87a618810a624afe5d
rnitta <ryushironitta@yahoo.co.jp>
2024-04-01 07:34:51 +0900

    canonical formsのプロトタイプ宣言

---
+++ src/server/src/RequestManager/RequestManager.hpp
@@ -0,0 +1,25 @@
+#ifndef REQUESTMANAGER_HPP
+# define REQUESTMANAGER_HPP
+
+namespace rnitta
+{
+
+
+class RequestManager
+{
+	public:
+		// constructor
+		RequestManager();
+		RequestManager( const RequestManager& other );
+		~RequestManager();
+		RequestManager&	operator=( const RequestManager& other );
+
+	private:
+
+};	// class RequestManager
+
+
+};	// namespace rnitta
+
+
+#endif
